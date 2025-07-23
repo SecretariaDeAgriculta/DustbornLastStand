1bb0936f9e11b45672cc73b67f92238ef283a34b
conta teste <contateste6196924@gmail.com>
2025-07-23 10:50:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/game/systems/moneyOrbSystem.ts
+++ src/game/systems/moneyOrbSystem.ts
@@ -1,10 +1,15 @@
 
-import type { MoneyOrbData, Player } from '../types';
+'use client';
+
+import type { MoneyOrbData } from '../types';
+import { useGameStore } from '@/store/useGameStore';
 import { MONEY_COLLECTION_RADIUS_SQUARED } from '../constants/game';
 
-export const updateMoneyOrbs = (currentOrbs: MoneyOrbData[], player: Player) => {
+export const updateMoneyOrbs = () => {
+    const { player, moneyOrbs, setMoneyOrbs, setPlayerDollars } = useGameStore.getState();
+
     const collectedOrbValues: number[] = [];
-    const remainingOrbs = currentOrbs.filter((orb) => {
+    const remainingOrbs = moneyOrbs.filter((orb) => {
       const distX = (player.x + player.width / 2) - (orb.x + orb.size / 2);
       const distY = (player.y + player.height / 2) - (orb.y + orb.size / 2);
       if (distX * distX + distY * distY < MONEY_COLLECTION_RADIUS_SQUARED) {
@@ -14,7 +19,10 @@ export const updateMoneyOrbs = (currentOrbs: MoneyOrbData[], player: Player) => 
     
     const collectedValue = collectedOrbValues.reduce((s, v) => s + v, 0);
 
-    return { remainingOrbs, collectedValue };
+    if (remainingOrbs.length !== moneyOrbs.length) {
+        setMoneyOrbs(remainingOrbs);
+    }
+    if (collectedValue > 0) {
+        setPlayerDollars(useGameStore.getState().playerDollars + collectedValue);
+    }
 };
-
-    
